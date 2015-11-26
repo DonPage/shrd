05b380ce1b75f901afa7dfd8980e919fd4c8671c
DonPage <don.page.work@gmail.com>
2015-11-25 22:27:17 -0500

    REMOVED: unused endpoints within ShrdGames. REMOVED: ShrdGame socket.

--- server/api/ShrdGame/ShrdGame.controller.js
+++ server/api/ShrdGame/ShrdGame.controller.js
@@ -20,40 +20,6 @@ exports.show = function(req, res) {
   });
 };
 
-// Creates a new ShrdGame in the DB.
-exports.create = function(req, res) {
-  ShrdGame.create(req.body, function(err, ShrdGame) {
-    if(err) { return handleError(res, err); }
-    return res.json(201, ShrdGame);
-  });
-};
-
-// Updates an existing ShrdGame in the DB.
-exports.update = function(req, res) {
-  if(req.body._id) { delete req.body._id; }
-  ShrdGame.findById(req.params.id, function (err, ShrdGame) {
-    if (err) { return handleError(res, err); }
-    if(!ShrdGame) { return res.send(404); }
-    var updated = _.merge(ShrdGame, req.body);
-    updated.save(function (err) {
-      if (err) { return handleError(res, err); }
-      return res.json(200, ShrdGame);
-    });
-  });
-};
-
-// Deletes a ShrdGame from the DB.
-exports.destroy = function(req, res) {
-  ShrdGame.findById(req.params.id, function (err, ShrdGame) {
-    if(err) { return handleError(res, err); }
-    if(!ShrdGame) { return res.send(404); }
-    ShrdGame.remove(function(err) {
-      if(err) { return handleError(res, err); }
-      return res.send(204);
-    });
-  });
-};
-
 function handleError(res, err) {
   return res.send(500, err);
 }
