7928955cc0e7f93e6e04d2521a0552e374d88f37
DonPage <don.page.work@gmail.com>
2015-12-23 11:46:34 -0600

    ADDED: hotjoin bindings and join function.

--- client/app/hotjoin/hotjoin.controller.js
+++ client/app/hotjoin/hotjoin.controller.js
@@ -4,12 +4,16 @@
 
   class HotJoinController {
 
-    constructor($http) {
-      this.games = [];
-      $http.get('/api/rooms/hotjoin')
-        .then(results => this.games = results.data)
+    constructor($http, $location) {
+      this.$location = $location;
+      this.rooms = $http.get('/api/rooms/hotjoin')
+        .then(results => this.rooms = results.data)
+        .catch(() => this.rooms = [])//TODO: make error response
     }
 
+    join(id) {
+      this.$location.path(`/room/${id}`)
+    }
   }
 
   angular.module('shrd2App')
